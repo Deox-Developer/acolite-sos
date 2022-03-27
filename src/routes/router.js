cc516b40f2f7c2e818ac519571951007e3c122a1
Nicole-Titumaita <71206667+Nicole-Titumaita@users.noreply.github.com>
2022-03-27 17:22:27 -0500

    DASHBOARD

---
+++ src/routes/router.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const {vistaPrincipal, vistaTables, vistaNotifications}= require('../controllers/PageControllers')
+const router = express.Router()
+
+//const conexion = require ('../database/db')
+
+
+router.get('/',vistaPrincipal)
+router.get('/tables',vistaTables)
+// router.get('/tables',(req , res)=>{
+
+//     conexion.query('SELECT *FROM usuarios', (error, results)=>{
+//         if (error) {
+//             throw error
+//         } else{
+//             res.render('tables',{results:results})
+//         }
+//     })
+// })
+router.get('/notifications',vistaNotifications)
+
+
+
+
+
+
+
+module.exports={routes: router}
