761ca86f4966052c8ce33afe96f210ed6009e247
Nicole-Titumaita <71206667+Nicole-Titumaita@users.noreply.github.com>
2022-04-11 11:40:14 -0500

    fix

--- main.js
+++ main.js
@@ -1,56 +1,39 @@
 const express = require('express');
 const morgan = require('morgan');
-const passport = require('passport');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const path = require('path')
-const expressLayouts = require('express-ejs-layouts')
-
-
-
+const hbs = require('hbs')
 
 //Inicialización de express
 const app = express();
 
-// app.set('view engine', 'ejs')
-// app.use(expressLayouts)
-//Archivos estaticos
-app.use(express.static(__dirname, + '/public'));
-
-
 //Configuraciones
 app.set('port', process.env.PORT || 8080);
-app.set('view engine', 'ejs');
-app.set('views',__dirname+ '/src/views');
+hbs.registerPartials(__dirname + '/src/views/partials', function (err) { });
+app.set('view engine', 'hbs');
+app.set("views", __dirname + "/src/views");
 
+app.use(express.static(__dirname + "/src/public"));
 
 //Middlewares
 app.use(morgan('dev'));
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extends: true }));
-app.use(cookieParser('Estes es mi secreto'));
-app.use(session({
-    secret: 'Estes es mi secreto',
-    resave: true,
-    saveUninitialized: true
-}));
 
-app.use(passport.initialize());
-app.use(passport.session());
 
+// Index
+app.get('/',(req, res)=>{res.render('index',{titulo: 'Home'})});
+app.use(require('./src/routes/nosotros.routers'));
+app.use(require('./src/routes/contacto.routers'));
 
 //Routes 
-app.use(require('./src/routes/home-router'));
-app.use(require('./src/routes/auth-router'))
-app.use(require('./src/routes/usuario.router'));
-// app.use(require('./src/routes/dashboard'));
 
+app.use(require('./src/routes/usuario.router'));
+app.use(require('./src/routes/auth.router'));
+app.use(require('./src/routes/dashboard.router'));
+
+
+app.use((req, res, next) => {res.status(404).render("404", {titulo: "404", descripcion: "Página no encontrada"})});
 
 //Iniciar Server
 app.listen(app.get('port'), () => {
-    console.log('Server on port', app.get('port'));
+    console.log(`Bienvenido a la consola http://localhost:${app.get("port")}`);
 });
-
-
