0b273af8c38eed75f5db07cf5d3e789f2f025df5
Jonathan Zambrano <deox.desing@gmail.com>
2022-04-17 00:42:04 -0500

    fix clientes, perfil,login

--- src/routes/dashboard.router.js
+++ src/routes/dashboard.router.js
@@ -1,9 +1,12 @@
 const {Router} =require('express');
 const router = Router();
 
-const {sessionUsuario} = require('../controllers/dashboard/dashboard.controller');
+const {sessionUsuario,perfilUsuario,clienteVehiculo,nuevoCliente} = require('../controllers/dashboard/dashboard.controller');
 
 router.get('/dashboard/perfil/:idUsuario/:nombre_usuario', sessionUsuario);
+router.get('/dashboard/perfil-usuario/:idSession/:nombre_usuario',perfilUsuario);
+router.get('/dashboard/cliente-vehiculo/:idSession/:nombre_usuario',clienteVehiculo);
+router.post('/dashboard/cliente-vehiculo/registrar-cliente',nuevoCliente)
 
 
 module.exports = router;
