4896792583cbc276314f63b8c8482e23b97a4aa6
Nicole-Titumaita <71206667+Nicole-Titumaita@users.noreply.github.com>
2022-04-26 09:20:09 -0500

    fixed

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -3,11 +3,7 @@ const {Pool} = require('pg');
 const pool = new Pool({
     host: 'localhost',
     user: 'postgres',
-<<<<<<< HEAD
     password: '1309',
-=======
-    password: '1234',
->>>>>>> Dylan-Lucio
     database: 'acoliteBD',
     port:'5432'
 });
