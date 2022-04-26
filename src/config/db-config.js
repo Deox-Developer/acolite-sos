38bfa780a51b161c02f420ba5da5c5685a4b961d
HakuAlejo <dag.lucio@yavirac.edu.ec>
2022-04-26 08:53:58 -0500

    Nuevos cambios

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -3,7 +3,11 @@ const {Pool} = require('pg');
 const pool = new Pool({
     host: 'localhost',
     user: 'postgres',
+<<<<<<< HEAD
+    password: '1309',
+=======
     password: '1234',
+>>>>>>> Dylan-Lucio
     database: 'acoliteBD',
     port:'5432'
 });
